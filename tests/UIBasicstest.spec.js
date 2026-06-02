f56efa0af5c221975141570e1c15ec07594fcfb1
D'Angelo Thomas <dangelo1thomas@gmail.com>
2026-06-01 21:01:27 -0500

    few tests added UI controls Web Client APP login

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -60,12 +60,49 @@ test('Hacker News Site', async ({page}) =>
 {
       await page.goto('https://news.ycombinator.com/');
 
-    console.log(await page.title());
-    
-
+    await page.waitForLoadState('networkidle')
+    //await page.locator('.titleline').last().waitFor(); -- alternate option
     const allTitles = await page.locator('.titleline').allTextContents();
 
     console.log(allTitles);
    
+});
+
+test('Web Client App login', async ({ page }) => {
+   //js file- Login js, DashboardPage
+   const email = "anshika@gmail.com";
+   const productName = 'zara coat 3';
+   const products = page.locator(".card-body");
+   await page.goto("https://rahulshettyacademy.com/client");
+   await page.locator("#userEmail").fill(email);
+   await page.locator("#userPassword").fill("Iamking@000");
+   await page.locator("[value='Login']").click();
+   await page.waitForLoadState('networkidle');
+   await page.locator(".card-body b").first().waitFor();
+   const titles = await page.locator(".card-body b").allTextContents();
+   console.log(titles); 
+ 
+});
+
+test('UI CONTROLS', async ({page}) => {
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    const userName = page.locator('#username');
+    const signIn = page.locator('#signInBtn');
+    const dropDown = page.locator('select.form-control');
+    await dropDown.selectOption("consult");
+    await page.locator('.radiotextsty').last().click();
+    await page.locator('#okayBtn').click();
+      //assertions
+      console.log(await page.locator('.radiotextsty').last().isChecked())
+    await expect(page.locator('.radiotextsty').last()).toBeChecked();
+    await page.locator("#terms").click();
+    await expect(page.locator('#terms')).toBeChecked();
+    await page.locator("#terms").uncheck();
+    console.log(await page.locator("#terms").isChecked());
+    expect(await page.locator("#terms").isChecked()).toBeFalsy();
+
+
 });
 
