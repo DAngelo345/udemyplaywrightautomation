232c5490280375218b7751d9ef7b6a5175cf64c6
D'Angelo Thomas <dangelo1thomas@gmail.com>
2026-06-28 17:53:07 -0500

    framesLocator used and .split

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -17,8 +17,14 @@ await expect(page.locator("#displayeed-text")).toBeHidden();
 //dialog.dissmiss() is also and option
 page.on('dialog', dialog => dialog.accept() );
 await page.locator('#confirmbtn').click();
+// lets you hover over an item then select for the options displayed
 await page.locator('#mousehover').hover();
 
+//how to selecte frames within a webstie basically a website within a website
+const framesPage = page.frameLocator("#courses-iframe");
+
+await framesPage.locator("le a[href*='lifetime-access']:visible").click();
+const textCheck = await framesPage.locator(".text h2").textConetent();
 
 
 })
